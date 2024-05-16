1ff0487efa29f415111ce68d9f0d7ead56e86e9c
Matheus Silva <msilvaqs@outlook.com>
2024-05-16 15:38:10 -0400

    changin instance type and key pair

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,20 +1,15 @@
 resource "aws_instance" "idream-jenkins-instance" {
   ami                    = "ami-0e001c9271cf7f3b9"
-  instance_type          = "t2.micro"
+  instance_type          = "t2.small"
   availability_zone      = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.idream-jenkins-instance-sg.id]
-  key_name               = aws_key_pair.idream-jenkins-instance-kp.key_name
+  key_name               = "idream-jenkins-instance-kp"
   user_data              = file("${path.module}/install_jenkins.sh")
 
   tags = {
     Name    = "idream-jenkins-instance-${var.ENVIRONMENT}"
     Project = "IDream"
   }
-}
-
-resource "aws_key_pair" "idream-jenkins-instance-kp" {
-  key_name   = "idream-jenkins-instance-kp-${var.ENVIRONMENT}"
-  public_key = file("${path.module}/aws_kp-${var.ENVIRONMENT}.pub")
 }
 
 resource "aws_eip" "idream-jenkins-instance-eip" {
