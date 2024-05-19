3da4b57ba6a1a9695c425818bd5da7e8047635a2
Matheus Silva <msilvaqs@outlook.com>
2024-05-19 15:39:40 -0400

    Including VPC

--- modules/beanstalk/main.tf
+++ modules/beanstalk/main.tf
@@ -1,3 +1,40 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_iam_role" "idream-beanstalk-role" {
+  name = "idream-beanstalk-role-${var.ENVIRONMENT}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "elasticbeanstalk.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkManagedUpdatesCustomerRolePolicy",
+    "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
+  ]
+}
+
+resource "aws_iam_instance_profile" "idream-beanstalk-instance-profile" {
+  name = "idream-beanstalk-instance-profile-${var.ENVIRONMENT}"
+  role = aws_iam_role.idream-beanstalk-role.name
+}
+
 resource "aws_elastic_beanstalk_application" "idream-beanstalk" {
   name = "idream-beanstalk-${var.ENVIRONMENT}"
   description = "Elastic Beanstalk Application for IDream ${var.ENVIRONMENT}"
@@ -14,9 +51,9 @@ resource "aws_elastic_beanstalk_environment" "idream-beanstalk-nodejs-env" {
   solution_stack_name = "64bit Amazon Linux 2023 v6.1.5 running Node.js 20"
 
   setting {
-      namespace = "aws:autoscaling:launchconfiguration"
-      name = "IamInstanceProfile"
-      value = "aws-elasticbeanstalk-ec2-role"
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.idream-beanstalk-instance-profile.name
   }
 
   setting {
@@ -35,5 +72,17 @@ resource "aws_elastic_beanstalk_environment" "idream-beanstalk-nodejs-env" {
     namespace = "aws:autoscaling:asg"
     name      = "MaxSize"
     value     = "1"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = data.aws_vpc.default.id
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", data.aws_subnets.default.ids)
   }
 }
