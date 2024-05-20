751d4d1be2049bbf1ac819c7bc78a0f2259ff861
Matheus Silva <msilvaqs@outlook.com>
2024-05-19 21:22:07 -0400

    Another try to create beanstalk

--- modules/beanstalk/main.tf
+++ modules/beanstalk/main.tf
@@ -1,88 +1,116 @@
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+# S3 bucket for Elastic Beanstalk
+resource "aws_s3_bucket" "beanstalk_bucket" {
+  bucket = "my-beanstalk-bucket" # change to a unique bucket name
+  acl    = "private"
+  
+  tags = {
+    Name = "beanstalk-bucket"
   }
 }
 
-resource "aws_iam_role" "idream-beanstalk-role" {
-  name = "idream-beanstalk-role-${var.ENVIRONMENT}"
+# Upload default HTML file to S3 bucket
+resource "aws_s3_bucket_object" "default_html" {
+  bucket = aws_s3_bucket.beanstalk_bucket.bucket
+  key    = "index.html"
+  source = "index.html"
+  acl    = "public-read" # change as needed for your security requirements
+
+  tags = {
+    Name = "DefaultHTML"
+  }
+}
+
+# SQS Queue
+resource "aws_sqs_queue" "beanstalk_queue" {
+  name = "beanstalk-queue"
+}
+
+# IAM role and policy for Elastic Beanstalk
+resource "aws_iam_role" "beanstalk_role" {
+  name = "beanstalk-role"
+
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow",
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
         Principal = {
           Service = "elasticbeanstalk.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
+        }
       }
     ]
   })
-
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AWSElasticBeanstalkManagedUpdatesCustomerRolePolicy",
-    "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
-  ]
 }
 
-resource "aws_iam_instance_profile" "idream-beanstalk-instance-profile" {
-  name = "idream-beanstalk-instance-profile-${var.ENVIRONMENT}"
-  role = aws_iam_role.idream-beanstalk-role.name
+resource "aws_iam_role_policy" "beanstalk_policy" {
+  name = "beanstalk-policy"
+  role = aws_iam_role.beanstalk_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:*",
+          "sqs:*",
+          "ec2:*",
+          "elasticloadbalancing:*",
+          "autoscaling:*",
+          "cloudwatch:*",
+          "logs:*",
+          "cloudformation:*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
 }
 
-resource "aws_elastic_beanstalk_application" "idream-beanstalk" {
-  name = "idream-beanstalk-${var.ENVIRONMENT}"
-  description = "Elastic Beanstalk Application for IDream ${var.ENVIRONMENT}"
-
-  tags = {
-    Name    = "idream-beanstalk-${var.ENVIRONMENT}"
-    Project = "IDream-${var.ENVIRONMENT}"
-  }
+# Elastic Beanstalk Application
+resource "aws_elastic_beanstalk_application" "beanstalk_app" {
+  name        = "my-app"
+  description = "My Elastic Beanstalk Application"
 }
 
-resource "aws_elastic_beanstalk_environment" "idream-beanstalk-nodejs-env" {
-  name                = "idream-beanstalk-nodejs-env-${var.ENVIRONMENT}"
-  application         = aws_elastic_beanstalk_application.idream-beanstalk.name
-  solution_stack_name = "64bit Amazon Linux 2023 v6.1.5 running Node.js 20"
+# Elastic Beanstalk Environment
+resource "aws_elastic_beanstalk_environment" "beanstalk_env" {
+  name                = "my-app-env"
+  application         = aws_elastic_beanstalk_application.beanstalk_app.name
+  solution_stack_name = "64bit Amazon Linux 2023 v6.1.5 running Node.js 20" # change as per your application's stack
 
   setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = aws_iam_instance_profile.idream-beanstalk-instance-profile.name
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "QUEUE_NAME"
+    value     = aws_sqs_queue.beanstalk_queue.name
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "BUCKET_NAME"
+    value     = aws_s3_bucket.beanstalk_bucket.bucket
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "EnvironmentType"
-    value     = "LoadBalanced" 
+    value     = "LoadBalanced"
   }
 
   setting {
-    namespace = "aws:autoscaling:asg"
-    name      = "MinSize"
-    value     = "1"
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_role.beanstalk_role.name
   }
+}
 
-  setting {
-    namespace = "aws:autoscaling:asg"
-    name      = "MaxSize"
-    value     = "1"
-  }
+# Output the S3 bucket name
+output "s3_bucket_name" {
+  value = aws_s3_bucket.beanstalk_bucket.bucket
+}
 
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "VPCId"
-    value     = data.aws_vpc.default.id
-  }
-
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "Subnets"
-    value     = "subnet-0230279177957baf8, subnet-04328d29ec743147d"
-  }
+# Output the SQS queue URL
+output "sqs_queue_url" {
+  value = aws_sqs_queue.beanstalk_queue.url
 }
